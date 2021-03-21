a4a520ee2588d0a7f5ea0d8ace1012703f9098cb
SS07 <73803477+SS0706@users.noreply.github.com>
2021-03-22 00:05:46 +0800

    Sorry forgot to migrate and changed import error

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Order, Report, NonFBChecklist, CovidChecklist
+from .models import Order, Report, NonFBChecklist, CovidComplianceChecklist, FBChecklist
 
 class RectifyForm(forms.ModelForm):
     class Meta:
