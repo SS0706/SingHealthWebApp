8aa7d1d33d4690c8638806dd66f7def05055afc7
SS07 <73803477+SS0706@users.noreply.github.com>
2021-03-24 19:34:35 +0800

    csv export partial

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-
+# from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-
+from django.http import HttpResponse, HttpResponseForbidden
+from .actions import export_as_csv_action
 from .models import *
+
+class RectifyAdmin(admin.ModelAdmin):
+    list_display = ['store', 'report', 'date_created']
+    actions = [export_as_csv_action("CSV Export", fields=['wink','nudge'])]
 
 admin.site.register(Report)
 admin.site.register(Store)
 admin.site.register(Tag)
-admin.site.register(Order)
+admin.site.register(Order, RectifyAdmin)
 admin.site.register(NonFBChecklist)
 admin.site.register(Announcement)
 admin.site.register(CovidComplianceChecklist)
